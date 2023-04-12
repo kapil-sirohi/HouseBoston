1ecefbf50f25787051f9296966cd6b87ff05e106
Kapil Sirohi <kapilsirohi25@gmail.com>
2023-04-12 16:39:20 +0530

    Web App ready

--- app.py
+++ app.py
@@ -24,5 +24,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print(data)
+    new_data = scaler.transform(np.array(data).reshape(1,-1))
+    output = regModel.predict(new_data)[0]
+    return render_template("home.html",prediction_text="The house cost you is {}".format(output))
+
 if __name__ == '__main__':
     app.run(debug=True)
